2703274134f4f4fd6ca74c22079a57955ce4a093
Anthony Carafa <124631913+AnthonyCarafa@users.noreply.github.com>
2025-12-30 03:03:10 -0500

    Enhance motor control with time parameter
    Added optional time parameter to forward and backwards methods for timed motor control.

--- main.py
+++ main.py
@@ -7,15 +7,27 @@ class Motor:
         self.in_pin_1 = DigitalOutputDevice(in_pin_1)
         self.in_pin_2 = DigitalOutputDevice(in_pin_2)
 
-    def forward(self, speed=1):
-        self.en.value = speed
-        self.in_pin_1.on()
-        self.in_pin_2.off()
+    def forward(self, speed=1, time=0): # time being set to 0 = infinite
+        if time == 0:
+            self.en.value = speed
+            self.in_pin_1.on()
+            self.in_pin_2.off()
+        else:
+            self.en.value = speed
+            self.in_pin_1.on()
+            self.in_pin_2.off()
+            sleep(time)
 
-    def backwards(self, speed=1):
-        self.en.value = speed
-        self.in_pin_1.off()
-        self.in_pin_2.on()
+    def backwards(self, speed=1, time=0): # time being set to 0 = infinite
+        if time == 0:
+            self.en.value = speed
+            self.in_pin_1.off()
+            self.in_pin_2.on()
+        else:
+            self.en.value = speed
+            self.in_pin_1.off()
+            self.in_pin_2.on()
+            sleep(time)
 
     def stop(self):
         self.en.value = 0
@@ -27,6 +39,4 @@ class Motor:
 
 test = Motor(1, 2, 3) # replace with real values
 
-test.forward()
-sleep(5)
-test.backwards()
+test.forward(1, 5) # max speed for 5 seconds, then motor shuts off
